dc5f7e6b88103a24657dad1afb89c2c132528772
anastakozz <shvedagon@gmail.com>
2023-04-30 16:50:03 +0300

    feat: add basic html generator

---
+++ index.js
@@ -0,0 +1,18 @@
+import {init} from './js/basicHtmlGenerator.js';
+
+
+
+const VirtualKeyboard = {
+    elements: {
+        main: null,
+        wrapper: null,
+        heading: null,
+        textarea: null,
+        keyboard: null,
+        keys: []
+    }
+}
+
+
+
+init(VirtualKeyboard);
---
+++ js/basicHtmlGenerator.js
@@ -0,0 +1,39 @@
+/**
+ * Creates basic html layout for the keyboard
+ * 
+ * @return {html structure}
+ */
+
+
+
+export function init(obj) {
+//create and setup elements
+obj.elements.main = document.createElement('div');
+obj.elements.main.classList.add('main'); 
+
+obj.elements.wrapper = document.createElement('div');
+obj.elements.wrapper.classList.add("wrapper");
+
+obj.elements.heading = document.createElement('h1');
+obj.elements.heading.innerText = 'RSS Virtual Keyboard';
+obj.elements.heading.classList.add("heading");
+
+obj.elements.textarea = document.createElement('textarea');
+obj.elements.textarea.classList.add("text-area");
+obj.elements.textarea.rows = 15;
+
+obj.elements.keyboard = document.createElement('div');
+obj.elements.keyboard.classList.add("keyboard");
+
+//create structure of elements
+
+obj.elements.main.appendChild(obj.elements.wrapper);
+obj.elements.wrapper.appendChild(obj.elements.heading);
+obj.elements.wrapper.appendChild(obj.elements.textarea);
+obj.elements.wrapper.appendChild(obj.elements.keyboard);
+
+
+//append elements to DOM
+    document.body.appendChild(obj.elements.main);   
+}
+
